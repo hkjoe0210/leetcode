b1d8102831e2e2ffa19cc08bdbb71fae3226e89b
hkjoe0210 <99551598+hkjoe0210@users.noreply.github.com>
2022-02-14 21:54:17 +0800

    Time: 9 ms (33.33%), Space: 20.4 MB (33.33%) - LeetHub

---
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.php
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[][]
+     */
+    function levelOrderBottom($root) {
+        $this->ans = array();
+        $this->getVal($root, 0);
+        $this->ans = array_reverse($this->ans);
+        return $this->ans;
+    }
+    function getVal($node, $level){
+        if(isset($node->val)){
+            $this->ans[$level][] = $node->val;
+            $this->getVal($node->left, $level + 1);
+            $this->getVal($node->right, $level + 1);
+        }
+    }
+}
