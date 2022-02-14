1807e9fcf55a14d0cf80355f69f6e03769319171
hkjoe0210 <99551598+hkjoe0210@users.noreply.github.com>
2022-02-14 22:30:34 +0800

    Time: 60 ms (50.00%), Space: 21.7 MB (11.90%) - LeetHub

---
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.php
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $val
+     * @return TreeNode
+     */
+    function insertIntoBST($root, $val) {
+        if(!$root){
+            return new TreeNode($val);
+        }
+        if($root->val > $val){
+            $root->left = $this->insertIntoBST($root->left, $val);
+        }else{
+            $root->right = $this->insertIntoBST($root->right, $val);
+        }
+        return $root;
+    }
+}
