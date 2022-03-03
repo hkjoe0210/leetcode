4d4935d6bbf654895c15f5b417b3280c3cf38528
hkjoe0210 <99551598+hkjoe0210@users.noreply.github.com>
2022-03-03 18:23:31 +0800

    Time: 243 ms (62.01%), Space: 21.2 MB (19.05%) - LeetHub

--- 1-two-sum/1-two-sum.php
+++ 1-two-sum/1-two-sum.php
@@ -6,12 +6,30 @@ class Solution {
      * @return Integer[]
      */
     function twoSum($nums, $target) {
-        for($i=0;$i<count($nums);$i++){
-            for($j=$i+1;$j<count($nums);$j++){
-                if( $nums[$i] + $nums[$j] === $target ){
-                    return array($i,$j);
+        asort($nums);
+        $nums_tmp = $nums;
+        $left = array_shift($nums_tmp);
+        $right = array_pop($nums_tmp);
+        $first = true;
+        $left_shift_count = 1;
+        while($nums_tmp || true){
+            if($left + $right == $target){
+                $left_key = array_search($left, $nums);
+                print_r($nums);
+                $right_key = array_search($right, array_slice($nums, $left_shift_count, null, true));
+                if($left_key == $right_key){
+                    $right_key++;
+                }
+                return array($left_key, $right_key);
+            }else{
+                if($left + $right > $target){
+                    $right = array_pop($nums_tmp);
+                }else{
+                    $left = array_shift($nums_tmp);
+                    $left_shift_count++;
                 }
             }
+            $first = false;
         }
     }
 }
